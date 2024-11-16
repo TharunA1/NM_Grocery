2f55f3c9ad3505763228b7022f099111614c20f1
TharunA1 <fireblaster4444@gmail.com>
2024-11-16 17:33:27 +0530

    updated

---
+++ server/src/db/connect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+// Middleware
+const db = 'mongodb+srv://tharun:tharun18@nm.tkwrg.mongodb.net/practice?retryWrites=true&w=majority&appName=NM'
+
+
+// Connect to MongoDB using the connection string
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log(`Connection successful`);
+}).catch((e) => {
+  console.log(`No connection: ${e}`);
+});
+
+// mongodb://localhost:27017
